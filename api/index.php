253004e8ecbaf4a72390019fb7cd16ceeaa66ce9
Barry van Oven <bvoven@gmail.com>
2012-01-09 00:17:50 +0100

    Some preparations before going public.

--- api/index.php
+++ api/index.php
@@ -133,8 +133,8 @@ function findByName($query) {
 function getConnection() {
 	$dbhost="127.0.0.1";
 	$dbuser="root";
-	$dbpass="alantin76";
-	$dbname="playground";
+	$dbpass="";
+	$dbname="library";
 	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $dbh;
